df7180eb4b50d46259cbea0a7ad24dcddfe72824
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-09-30 17:42:54 +0000

    video attribs added

--- src/main/java/is.ru.honn.rutube/domain/Video.java
+++ src/main/java/is.ru.honn.rutube/domain/Video.java
@@ -1,7 +1,15 @@
 package is.ru.honn.rutube.domain;
 
+import java.util.List;
+
 /**
- * Created by Snojo on 30/09/16.
+ * Created by krihf & Snojo on 30/09/16.
  */
 public class Video {
+    int videoId;
+    String title;
+    String description;
+    String src;
+    String type;
+    List<String> tags;
 }
