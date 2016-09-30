2f02c8404c1f195ca1da02a3c804043a9190aa38
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-09-30 19:32:30 +0000

    working on videos and the service stub

--- src/main/java/is.ru.honn.rutube/domain/Video.java
+++ src/main/java/is.ru.honn.rutube/domain/Video.java
@@ -6,10 +6,24 @@ import java.util.List;
  * Created by krihf & Snojo on 30/09/16.
  */
 public class Video {
-    int videoId;
-    String title;
-    String description;
-    String src;
-    String type;
-    List<String> tags;
+
+    private int videoId;
+    public String title;
+    public String description;
+    public String src;
+    public String type;
+    public List<String> tags;
+
+    public Video(int videoId, String title, String description, String src, String type, List<String> tags) {
+        this.videoId = videoId;
+        this.title = title;
+        this.description = description;
+        this.src = src;
+        this.type = type;
+        this.tags = tags;
+    }
+
+    public int getVideoId() {
+        return videoId;
+    }
 }
--- src/main/java/is.ru.honn.rutube/service/VideoServiceStub.java
+++ src/main/java/is.ru.honn.rutube/service/VideoServiceStub.java
@@ -2,16 +2,31 @@ package is.ru.honn.rutube.service;
 
 import is.ru.honn.rutube.domain.ServiceException;
 import is.ru.honn.rutube.domain.Video;
-
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by krihf on 30.9.2016.
  */
 public class VideoServiceStub implements VideoService {
 
+    private ArrayList<Video> videos;
+
+    public VideoServiceStub()
+    {
+        videos = new ArrayList<Video>();
+    }
+
     public int addVideo(Video video, int userId) throws ServiceException
     {
+        for (Video v : videos) {
+            if(v.getVideoId() == video.getVideoId()) {
+                throw new ServiceException();
+            }
+        }
+
+        // here we presume that it's been checked that the user exists
+        // TODO: check if we need to add a pointer to the user service stub
+        videos.add(video);
         return 0;
     }
 
