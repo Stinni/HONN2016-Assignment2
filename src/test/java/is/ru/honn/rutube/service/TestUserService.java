8545a9f01edaa64d5da5c496092199b9dd3b8c9d
Stinni <kristinnf13@ru.is>
2016-10-01 05:48:57 +0000

    messing around because of .iml file problems

--- src/test/java/is/ru/honn/rutube/service/TestUserService.java
+++ src/test/java/is/ru/honn/rutube/service/TestUserService.java
@@ -25,3 +25,46 @@ public class TestUserService extends TestCase {
 
     }
 }
+
+/*
+package is.ru.honn.rutube.test;
+
+import org.junit.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.*;
+
+
+public class is.ru.honn.rutube.service.TestUserService {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    //private SalaryCalculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        //calculator = new SalaryCalculator();
+    }
+
+    @Test
+    public void test1_ExitsWithExitCode4() throws Exception {
+        /*int basePay = 450;
+        int hours = 35;
+        calculator.calculateSalary(basePay, hours);
+        assertEquals("Base pay can't be lower than the minimum wage (500)", outContent.toString());*//*
+    }
+
+    @Test
+    public void TestAddUser() throws Exception{
+
+    }
+}
+* */
+/**
+ * Hönnun og Smíði Hugbúnaðar - Assignment 2, Part 1:
+ * Unit test class is.ru.honn.rutube.service.TestUserService (is.ru.honn.rutube.service.TestUserService.java)
+ * for the UserService (UserService.java) class
+ *
+ * @author Kristinn Heiðar Freysteinsson & Snorri Hjörvar Jóhannsson
+ * @version 1, 30.09.16
+ */
