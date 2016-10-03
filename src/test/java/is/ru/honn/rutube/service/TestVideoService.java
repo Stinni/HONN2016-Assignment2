c0f7dad92d883e37e9f522fe42c46a4bb9f42fb4
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-10-03 16:11:58 +0000

    started on the videoservice tests

--- src/test/java/is/ru/honn/rutube/service/TestVideoService.java
+++ src/test/java/is/ru/honn/rutube/service/TestVideoService.java
@@ -1,7 +1,13 @@
 package is.ru.honn.rutube.service;
 
+import is.ru.honn.rutube.domain.User;
+import is.ru.honn.rutube.domain.Video;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by krihf on 3.10.2016.
@@ -20,12 +26,19 @@ Test if adding and getting videos works right
     @Before
     public void setUp() throws Exception {
         _uService = new UserServiceStub();
+        _uService.addUser(new User(12345678, "Magnus", "Bjornsson", "meb@ru.is", "MEB", "1975-10-03"));
         _vService = new VideoServiceStub(_uService);
+        List<String> tags = new ArrayList<>();
+        tags.add("woohoo"); tags.add("woowoo");
+        _vService.addVideo(new Video(22222222, "TwoTwoTwo", "Description", "url", "music", tags), 12345678);
     }
 
     @Test
-    public void testAddUser() throws Exception {
-
+    public void testAddVideo_test1() throws Exception {
+        List<String> tags = new ArrayList<>();
+        tags.add("threeeeee");
+        _vService.addVideo(new Video(33333333, "ThreeThree", "Description", "url", "music", tags), 12345678);
+        assertEquals(33333333, _vService.getVideo(33333333).getVideoId());
     }
 
     @Test
