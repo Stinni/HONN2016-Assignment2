8251fd5805d8febdc7a623e70bc469d88acc53a5
Snojo <snojo@simnet.is>
2016-10-05 14:35:25 +0000

    Finished Test for userservice

--- src/test/java/is/ru/honn/rutube/service/TestUserService.java
+++ src/test/java/is/ru/honn/rutube/service/TestUserService.java
@@ -1,5 +1,6 @@
 package is.ru.honn.rutube.service;
 
+import is.ru.honn.rutube.domain.ServiceException;
 import is.ru.honn.rutube.domain.User;
 import junit.framework.TestCase;
 import org.junit.Assert;
@@ -16,10 +17,10 @@ import static org.junit.Assert.assertTrue;
  */
 public class TestUserService extends TestCase {
     private UserServiceStub _uService;
+    ApplicationContext resource = new FileSystemXmlApplicationContext("/src/test/resources/spring-config.xml");
 
     @Before
     public void setUp() throws Exception {
-        ApplicationContext resource = new FileSystemXmlApplicationContext("/src/test/resources/spring-config.xml");
         _uService = new UserServiceStub();
         User u = (User)resource.getBean("user");
         _uService.addUser(u);
@@ -27,19 +28,37 @@ public class TestUserService extends TestCase {
 
     @Test
     public void testAddUser() throws Exception {
-        assertTrue(true);
+        User u2 = (User)resource.getBean("user2");
+        _uService.addUser(u2);
+        assertEquals(54321, _uService.getUser(54321).getUserId());
+    }
+
+    @Test
+    public void testErrorAddUser() throws Exception {
+        User u = (User)resource.getBean("user");
+        try{
+            _uService.addUser(u);
+        } catch(ServiceException e){
+            assertEquals("User already exists.", e.getMessage());
+        }
     }
 
     @Test
     public void testGetUser() throws Exception {
         User u = _uService.getUser(12345);
-        //assertEquals("Snorri", u);
-        assertTrue(true);
+        assertEquals("Snorri", u.getFirstName());
     }
 
     @Test
-    public void testGetUsers() throws Exception {
-        assertTrue(true);
+    public void testSetOperators() throws Exception {
+        User nu = new User();
+        nu.setFirstName("Boham");
+        nu.setLastName("Johnson");
+        nu.setUserId(66666);
+        _uService.addUser(nu);
+        assertEquals("Boham", nu.getFirstName());
+        assertEquals("Johnson", nu.getLastName());
+        assertEquals(66666, nu.getUserId());
     }
 }
 
