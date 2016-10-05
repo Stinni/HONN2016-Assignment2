69c913d048d4493b709abe43fe53cb44efe1c5b2
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-10-05 15:58:40 +0000

    bleh

--- src/main/java/is.ru.honn.rutube/reader/AbstractReader.java
+++ src/main/java/is.ru.honn.rutube/reader/AbstractReader.java
@@ -6,20 +6,28 @@ package is.ru.honn.rutube.reader;
 public abstract class AbstractReader implements Reader {
 
     private ClientRequest clientRequest;
+    private ReadHandler readHandler;
+    private String URI;
 
     public AbstractReader() {
         clientRequest = new ClientRequest();
+        URI = "http://mockaroo.com/f13b8200/download?count=1&key=e79a3650";
+    }
+
+    public AbstractReader(String uriString) {
+        clientRequest = new ClientRequest();
+        this.URI = uriString;
     }
 
     public Object read() {
-        return parse(clientRequest.getRequest("http://mockaroo.com/f13b8200/download?count=1&key=e79a3650"));
+        return parse(clientRequest.getRequest(URI));
     }
 
     public void setURI(String URI) {
-
+        this.URI = URI;
     }
 
     public void setReadHandler(ReadHandler readHandler) {
-
+        this.readHandler = readHandler;
     }
 }
